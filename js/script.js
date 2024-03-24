5f3f80e1668a07bcc5bd0478d8c0960c3e1cd025
Hall Family <charliesamhall@icloud.com>
2024-03-24 18:52:39 -0400

    random letter logic

--- js/script.js
+++ js/script.js
@@ -3,9 +3,31 @@
 //letters with no countries: X 
 
 const startBtn = document.getElementById('start-button')
+const letterDiv = document.getElementById('round-letter')
+const inputSection = document.getElementById('input-section')
+const buttonSection = document.getElementById('button-section')
+
+const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y', 'Z']
 
 const run = () => {
-    alert('hi')
+    startBtn.setAttribute('class', 'hidden')
+    const roundLetter = letters[Math.floor(Math.random() * letters.length)]
+
+    const letterDisplay = document.createElement('h1')
+    letterDisplay.textContent = `This round's letter is ${roundLetter}` 
+    letterDiv.appendChild(letterDisplay)
+
+    let countryCount = 3
+
+    console.log(roundLetter)
+
+    if(roundLetter === 'Z'){
+        countryCount = 2
+    } else if(roundLetter === 'O' || roundLetter === 'Q' || roundLetter ==='W' || roundLetter === 'Y' ){
+        countryCount = 1 
+    }
+
+    console.log(countryCount)
 }
 
 startBtn.addEventListener('click', run)
