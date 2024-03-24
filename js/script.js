74b9d95a15f1afba5a1362568c77d9dd562897aa
Hall Family <charliesamhall@icloud.com>
2024-03-24 18:10:02 -0400

    file commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//letters that have only two countries: Z
+//letters that have only one country: O, Q, W, Y, 
+//letters with no countries: X 
+
+const startBtn = document.getElementById('start-button')
+
+const run = () => {
+    alert('hi')
+}
+
+startBtn.addEventListener('click', run)
